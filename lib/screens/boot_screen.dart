1e856866a93741c330ccda06b19f68c31e9fecc5
Ryan Kobayashi <ryko.haelioe@gmail.com>
2020-06-13 17:40:27 -0700

    Initial Commit

---
+++ lib/screens/boot_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BootScreen extends StatefulWidget {
+  @override
+  _BootScreenState createState() => _BootScreenState();
+}
+
+class _BootScreenState extends State<BootScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
