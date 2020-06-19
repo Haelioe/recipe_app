05331666799aed724f94a3e263651705c1c82f0f
Ryan Kobayashi <ryko.haelioe@gmail.com>
2020-06-19 15:28:22 -0700

    TURBO hard coded bottom nav bar updated for fav screen and home screen

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:recipeapp/screens/profile_screen.dart';
+
+import 'home_screen.dart';
 
 class FavoritesScreen extends StatefulWidget {
   @override
@@ -8,6 +11,70 @@ class FavoritesScreen extends StatefulWidget {
 class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[],
+      ),
+      bottomNavigationBar: Row(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (_) => HomeScreen()));
+            },
+            child: Container(
+              height: 60,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width / 3,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: Icon(
+                Icons.home,
+                color: Colors.blue[300],
+              ),
+            ),
+          ),
+          Container(
+            height: 60,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width / 3,
+            decoration: BoxDecoration(
+              color: Colors.blue[100],
+            ),
+            child: Icon(
+              Icons.star,
+              color: Colors.blue[800],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (_) => ProfileScreen()));
+            },
+            child: Container(
+              height: 60,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width / 3,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: Icon(
+                Icons.person,
+                color: Colors.blue[300],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:recipeapp/screens/profile_screen.dart';
+
+import 'favorites_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +11,61 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[],
+      ),
+      bottomNavigationBar: Row(
+        children: <Widget>[
+          Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: Icon(
+                Icons.home,
+                color: Colors.blue[800],
+              ),
+            ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (_) => FavoritesScreen()));
+            },
+            child: Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: Icon(
+                Icons.star,
+                color: Colors.blue[300],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (_) => ProfileScreen()));
+            },
+            child: Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: Icon(
+                Icons.person,
+                color: Colors.blue[300],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
