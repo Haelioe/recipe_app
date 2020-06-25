c17af19acb770b033cfa1bcad103b0a3e16d3daf
Ryan Kobayashi <ryko.haelioe@gmail.com>
2020-06-24 17:58:49 -0700

    Honey Garlic screen instead - LOOKIN PRETTY CLEAN NGL

--- lib/screens/chicken_strog_screen.dart
+++ lib/screens/chicken_strog_screen.dart
@@ -15,8 +15,44 @@ class _ChickenStrogScreenState extends State<ChickenStrogScreen> {
       ),
       body: Stack(
         children: <Widget>[
-          Container(
-
+          Opacity(
+            opacity: .5,
+            child: Container(
+              height: MediaQuery.of(context).size.width * .75,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/chicken.jpg',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              SizedBox(height: 120),
+              Text(
+                'Chicken Stroganoff',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.grey[800]),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 100),
+              Container(
+                height: 400,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                    color: Colors.amber[100]),
+                child: Column(
+                  children: <Widget>[
+                  ],
+                ),
+              )
+            ],
           )
         ],
       ),
--- lib/screens/honey_chicken_screen.dart
+++ lib/screens/honey_chicken_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HoneyChicken extends StatefulWidget {
@@ -9,11 +11,88 @@ class _HoneyChickenState extends State<HoneyChicken> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("Honey Garlic Chicken"),
-        automaticallyImplyLeading: false,
-
+      appBar: AppBar(
+        title: Text("Honey Garlic Chicken"),
+        automaticallyImplyLeading: true,
       ),
-
+      body: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: .5,
+            child: Container(
+              height: MediaQuery.of(context).size.width * .75,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/honey.jpg',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              SizedBox(height: 120),
+              Text(
+                'Honey Garlic Chicken',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.grey[800]),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 100),
+              Container(
+                height: 400,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                    color: Colors.amber[100]),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Cook Time:',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[800],
+                                ),
+                              ),
+                              SizedBox(width: 10,),
+                              Text(
+                                '30 min',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red[400],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.hourglass_empty),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
